1a4a212b7c7cfb775e6327d03fe2b7d9088d51e0
Bruna Santos <brunarocha@id.uff.br>
2020-03-21 19:53:33 -0300

    Portfolio inicial

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Meu Currículo</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+</head>
+
+<body>
+
+<body>
+<div class="wrapper-masthead">
+    <div class="container">
+        <header class="masthead clearfix">
+            <a href="/" class="site-avatar"><img class="avatar" src="eu.jpg" /></a>
+
+            <div class="site-info">
+                <h1 class="site-name"><a href="/">Bruna Santos</a></h1>
+                <p class="site-description">Desenvolvedora mobile e WEB</p>
+            </div>
+        </header>
+    </div>
+</div>
+
+<div id="main" role="main" class="container">
+    <article class="page">
+
+        <h1>About</h1>
+
+        <div class="entry">
+            <p>Desenvolvedora WEB e Mobile formada na UFF</p>
+
+            <h3>Um pouco mais sobre mim</h3>
+
+            <p>Formada na UFF pelo curso de tecnologia em sistemas em computação, trabalho com desenvolvimento web utilizando a linguagem PHP.
+                Também realizo trabalhos vonluntários na comunidade PHP Women, nosso objetivo é a inclusão de mulheres no mercado da computação.
+                No Mobile, utilizo a linguagem reactv native para desenvolvimento de aplicativos multi-plataformas. Estou sempre aberta a novos conhecimentos e busco aprender sempre.
+                Pretento seguir a área de pesquisa no campo de IHC, área a qual descobrir no ultimo ano da faculdade e me apaixonei.</p>
+
+            <h3>Contact me</h3>
+
+            <p><a href="mailto:email@domain.com">brunassrocha@yahoo.com.br</a></p>
+
+        </div>
+    </article>
+
+</div>
+
+<div class="wrapper-footer">
+    <div class="container">
+        <footer class="footer">
+
+
+
+
+            <a href="http://github.com/rafaeltoledo"><i class="svg-icon github"></i></a>
+
+
+
+
+            <a href="http://twitter.com/_rafaeltoledo"><i class="svg-icon twitter"></i></a>
+
+
+        </footer>
+    </div>
+</div>
+
+
+
+</body>
+
+</html>
